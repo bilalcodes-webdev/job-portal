76b2a0ce498f3b79c808488f108aeec12872241d
bilalcodes-webdev <bilal.webdev.official@gmail.com>
2025-08-21 22:52:42 +0500

    add inngest

--- src/lib/inngest/client.ts
+++ src/lib/inngest/client.ts
@@ -1,4 +1,7 @@
 import { Inngest } from "inngest";
 
-// Create a client to send and receive events
-export const inngest = new Inngest({ id: "job-bilal" });
+// Server-side Inngest client
+export const inngest = new Inngest({
+  id: "job-bilal",
+  apiKey: process.env.INNGEST_EVENT_KEY, // <-- yaha event key use hoti hai
+});
